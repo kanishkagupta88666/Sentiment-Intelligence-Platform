9771c68ece0a22fc348cb4c39bbaf3a6da9b6ba2
Kanishka Gupta <kanishkagupta@illinoisnet-guest-10-200-84-22.near.illinois.edu>
2026-04-30 22:33:18 -0500

    Fix AgentExecutor import

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from sklearn.preprocessing import normalize
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.tools import Tool
-from langchain_core.agents import AgentExecutor
-from langchain.agents import create_react_agent
+from langchain.agents import AgentExecutor, create_react_agent
 from langchain import hub
 
 load_dotenv()
